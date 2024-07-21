b54f44fdaafde66bbf9e0cc4a05d3f8d245d862b
AdityaNaida <adityanaida@gmail.com>
2024-07-21 19:54:42 +0530

    [ADD] Navbar & ejs-mate

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 const Listing = require("./models/listening.js");
 
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
   .then(() => {
@@ -26,6 +27,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs", ejsMate);
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListning = new Listing({});
